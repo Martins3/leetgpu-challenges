ae34df7b9815f977c4cee8b19f6a35c7b121db57
Xueshi Hu <xueshi.hu@smartx.com>
2026-05-10 07:05:56 +0800

    1

--- challenges/easy/19_reverse_array/starter/starter.cu
+++ challenges/easy/19_reverse_array/starter/starter.cu
@@ -1,6 +1,17 @@
 #include <cuda_runtime.h>
 
-__global__ void reverse_array(float* input, int N) {}
+// 2
+__global__ void reverse_array(float* input, int N) {
+    size_t idx = static_cast<size_t>(blockIdx.x) * blockDim.x + threadIdx.x;
+    size_t exchange_idx = N - 1 - idx;
+    size_t middle = N / 2;
+    if (idx >= middle)
+        return;
+    // TODO 应该是存在原地切换的才可以的
+    size_t tmp = input[exchange_idx];
+    input[exchange_idx] = input[idx];
+    input[idx] = tmp;
+}
 
 // input is device pointer
 extern "C" void solve(float* input, int N) {
